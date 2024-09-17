9dd14484b2446e1ca8ebd1c74a411e8cf8bd8add
ThiKim148 <hothikim14102004@gmail.com>
2024-09-17 11:24:36 +0700

    ok

---
+++ midterm.js
@@ -0,0 +1,17 @@
+function add() {
+
+    var imageName = document.getElementById('img-name').value;
+    var prName = document.getElementById('pr-name').value; 
+    var newPrice = document.getElementById('newprice').value; 
+    var oldPrice = document.getElementById('oldprice').value;
+    var prType = document.getElementById('type').value; 
+    var proCode = document.getElementById('code').value;
+
+    document.getElementById('img-pr');
+    document.getElementById('name-pr');
+    document.getElementById('code-pr');
+    document.getElementById('pr');
+
+
+
+}
